ba6d1001babd4b90575665067b0715925717fc13
Ben Wolski <570819+benwolski@users.noreply.github.com>
2024-12-27 10:35:35 -0500

    update import

--- src/pages/FutaSection6.jsx
+++ src/pages/FutaSection6.jsx
@@ -1,13 +1,13 @@
-import image from '../assets/images/section6First.svg';
-import image2 from '../assets/images/section6Second.svg';
+import section6First from '../assets/images/section6First.svg';
+import section6Second from '../assets/images/section6Second.svg';
 import ImageDisplay from '../components/ImageDisplay/ImageDisplay';
-import styles from './Sections.module.css'
+import styles from './Sections.module.css';
 
 export default function FutaSection6() {
   return (
     <div className={styles.main} >
 
-          <ImageDisplay image={image} image2={image2} />
+          <ImageDisplay image={section6First} image2={section6Second} />
     </div>
   );
 }
