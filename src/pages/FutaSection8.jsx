05f2a6f4fb6dbee87b0fdb1ba48dde4db2e09842
Ben Wolski <570819+benwolski@users.noreply.github.com>
2024-12-27 10:33:52 -0500

    update name of imported svg

--- src/pages/FutaSection8.jsx
+++ src/pages/FutaSection8.jsx
@@ -1,13 +1,13 @@
-import image from '../assets/images/section8First.svg';
-import image2 from '../assets/images/section8Second.svg';
+import section8First from '../assets/images/section8First.svg';
+import section8Second from '../assets/images/section8Second.svg';
 import ImageDisplay from '../components/ImageDisplay/ImageDisplay';
-import styles from './Sections.module.css'
+import styles from './Sections.module.css';
 
 export default function FutaSection8() {
   return (
     <div className={styles.main} >
 
-          <ImageDisplay image={image} image2={image2} />
+          <ImageDisplay image={section8First} image2={section8Second} />
     </div>
   );
 }
